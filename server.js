8d7c8acee46eb856e7dd45697624a51308560ebb
RSushil1 <sushilsinghrathore1998@gmail.com>
2023-08-17 01:03:11 +0530

    ,,

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import http from 'http'; // Import the http module
+import https from 'https'; // Import the http module
 import { Server } from 'socket.io';
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
@@ -19,45 +19,50 @@ const PORT = process.env.PORT || 8000;
 
 //rest object
 const app = express();
-const server = http.createServer(app); // Create HTTP server instance
+const server = https.createServer(app); // Create HTTP server instance
 
 
 //middelwares
 app.use(cors({
-    origin: 'https://echochat.vercel.app'
-  }));
+  origin: 'https://echochat.vercel.app'
+}));
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://echochat.vercel.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // // Increase payload size limits for JSON and URL-encoded bodies
 // app.use(express.json({ limit: '10mb' }));
 // app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Socket.IO configuration
 const io = new Server(server, {
-    cors: {
-      origin: 'https://echochat.vercel.app',
-      methods: ['GET', 'POST'],
-      allowedHeaders: ['my-custom-header'],
-      credentials: true
-    }
-  }); // Pass the HTTP server instance to the Socket.IO Server
+  cors: {
+    origin: 'https://echochat.vercel.app',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true
+  }
+}); // Pass the HTTP server instance to the Socket.IO Server
 
-  io.on('connection', socket => {
-    const id = socket.handshake.query.id;
-    socket.join(id);
-    
-    socket.on('send-message', ({ recipients, text }) => {
-        recipients.forEach(recipient => {
-            const newRecipients = recipients.filter(r => r !== recipient);
-            newRecipients.push(id);
-            io.to(recipient).emit('receive-message', {
-                recipients: newRecipients,
-                sender: id,
-                content: text,
-                timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
-            });
-        });
+io.on('connection', socket => {
+  const id = socket.handshake.query.id;
+  socket.join(id);
+
+  socket.on('send-message', ({ recipients, text }) => {
+    recipients.forEach(recipient => {
+      const newRecipients = recipients.filter(r => r !== recipient);
+      newRecipients.push(id);
+      io.to(recipient).emit('receive-message', {
+        recipients: newRecipients,
+        sender: id,
+        content: text,
+        timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+      });
     });
+  });
 });
 
 
@@ -68,7 +73,7 @@ app.use("/api/auth", authRoutes);
 
 //run listen
 server.listen(PORT, () => {
-    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`);
+  console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`);
 });
 
 
