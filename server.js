6097bf4fc9f1d325f2b6270990d92ee02f2b0f4f
RSushil1 <sushilsinghrathore1998@gmail.com>
2023-08-17 01:10:55 +0530

    ,,

--- server.js
+++ server.js
@@ -28,11 +28,6 @@ app.use(cors({
 }));
 // app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://echochat.vercel.app"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 // Increase payload size limits for JSON and URL-encoded bodies
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
