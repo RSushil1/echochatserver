31ead9487f9d7153f899ebbfcb69b11b243645ad
RSushil1 <sushilsinghrathore1998@gmail.com>
2023-08-18 16:13:03 +0530

    ,,

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "my-custom-header");
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   next();
 });
@@ -41,30 +42,30 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Socket.IO configuration
 const io = new Server(server, {
-    cors: {
-      origin: 'https://echochat.vercel.app',
-      methods: ['GET', 'POST'],
-      allowedHeaders: ['my-custom-header'],
-      credentials: true
+  cors: {
+    origin: 'https://echochat.vercel.app',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+  credentials: true
     }
   }); // Pass the HTTP server instance to the Socket.IO Server
 
-  io.on('connection', socket => {
-    const id = socket.handshake.query.id;
-    socket.join(id);
-    
-    socket.on('send-message', ({ recipients, text }) => {
-        recipients.forEach(recipient => {
-            const newRecipients = recipients.filter(r => r !== recipient);
-            newRecipients.push(id);
-            io.to(recipient).emit('receive-message', {
-                recipients: newRecipients,
-                sender: id,
-                content: text,
-                timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
-            });
-        });
+io.on('connection', socket => {
+  const id = socket.handshake.query.id;
+  socket.join(id);
+
+  socket.on('send-message', ({ recipients, text }) => {
+    recipients.forEach(recipient => {
+      const newRecipients = recipients.filter(r => r !== recipient);
+      newRecipients.push(id);
+      io.to(recipient).emit('receive-message', {
+        recipients: newRecipients,
+        sender: id,
+        content: text,
+        timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+      });
     });
+  });
 });
 
 
@@ -75,7 +76,7 @@ app.use("/api/auth", authRoutes);
 
 //run listen
 server.listen(PORT, () => {
-    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`);
+  console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`);
 });
 
 
