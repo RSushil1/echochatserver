9ac25ed6e04927da32a0e630e0e080cb9d1de3b0
RSushil1 <sushilsinghrathore1998@gmail.com>
2023-08-18 16:15:22 +0530

    ,,

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors());
 
 // Middleware to set CORS headers
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://echochat.vercel.app");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Headers", "my-custom-header");
